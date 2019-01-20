dac1f8caead5933bea5d88369b4a82bcc151fe52
Gede Wahyu <tokekbesi@gmail.com>
2019-01-20 10:45:25 +0700

    feat: using http.ListenAndServe

---
+++ b1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+  var message = "Welcome"
+  w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+  var message = "Hello World"
+  w.Write([]byte(message))
+}
+
+func main() {
+  http.HandleFunc("/", handlerIndex)
+  http.HandleFunc("/index", handlerIndex)
+  http.HandleFunc("/hello", handlerHello)
+
+  var address = "localhost:9000"
+  fmt.Printf("server is started at %s\n", address)
+  err := http.ListenAndServe(address, nil)
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+}
